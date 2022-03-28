b3d9d154653e7682a83b415278bcffa7abde85e3
PetroLakhai <pete.lakhay@gmail.com>
2022-03-28 15:42:08 +0300

    slug

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from rango.models import Category, Title
-
-admin.site.register(Category)
-# admin.site.register(Title)
+from rango.models import Category, Page
 
 
-@admin.register(Title)
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Page)
 class PersonAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url")
 
