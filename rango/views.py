373b33b0fb4ab01484c9bb245179172fad8c51cc
PetroLakhai <pete.lakhay@gmail.com>
2022-04-22 16:35:42 +0300

    credentials

--- rango/views.py
+++ rango/views.py
@@ -70,6 +70,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -93,6 +94,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -223,7 +225,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
@@ -233,8 +235,3 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
-
-
-
-
-
